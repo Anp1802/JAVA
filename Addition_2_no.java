30dd2a497e17525b045b369df34a822a47fbe89b
Aditya Patil <71185387+Anp1802@users.noreply.github.com>
2020-09-13 00:09:58 +0530

    Add files via upload

---
+++ Addition_2_no.java
@@ -0,0 +1,9 @@
+public class Addition_2_no  {
+    public static void main(String args[]) {
+      int x=7;
+      int y=4;
+      int z=x+y;
+
+      System.out.println("Sum of x+y = " + z);
+    }
+}
